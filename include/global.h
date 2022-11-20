b35986c1e804a33b45bdd26aa5f3635418d3b212
DESCAMPS antoine <antoine.descamps1213@gmail.com>
2022-11-20 07:16:31 +0100

    Replace global.h

--- include/global.h
+++ include/global.h
@@ -1,6 +1,11 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+#define MAX_ARGS_NUMBER 4096
+#define MAX_ARGS_STRLEN 4096
+#define MAX_PWD_LENGTH 256
+
 extern int last_return_value;
+extern char logical_pwd[MAX_PWD_LENGTH];
 
 #endif
