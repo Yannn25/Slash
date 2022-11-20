521f77e7450b47a028d0eb98c340165ae9b9cf62
DESCAMPS antoine <antoine.descamps1213@gmail.com>
2022-11-20 01:36:11 +0100

    Upload New File

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "include/prompt_formatting.h"
+#include "include/internal_functions.h"
+#include "include/global.h"
+
+#define MAX_ARGS_NUMBER 4096
+#define MAX_ARGS_STRLEN 4096
+
+// paramètrage des variables globales
+int last_return_value = 0;
+
+int main(void)
+{
+	// initialisation
+	int proc;
+	rl_outstream = stderr;
+
+	// boucle principale
+	while(1)
+	{
+		// Prompt Slash
+		char* prompt = readline("$ ");
+		printf("prompt = %s\n", prompt);
+		add_history(prompt);
+
+		//On vérifie si l'utilisateur a saisi une commande interne
+		command* user_command = command_formatting(prompt);
+		// Lancement de la commande utilisateur
+		last_return_value = execute(user_command);
+
+		/* Sinon, on vérifie si l'utilisateur a saisi une commande externe
+		(à mettre plutot dans la fct execute)
+		if((proc = fork()) == 0)
+		{
+			execl("/bin/pwd", "pwd", NULL);
+		}
+		*/
+
+		// si ni l'un ni l'autre, message d'erreur et on boucle
+	}
+
+return 0;
+}
+
