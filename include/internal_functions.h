c891d69374aea482b0b1c4403095cf1bc7df07a1
DESCAMPS antoine <antoine.descamps1213@gmail.com>
2022-11-20 01:36:54 +0100

    Upload New File

---
+++ include/internal_functions.h
@@ -0,0 +1,25 @@
+#ifndef INTERNAL_FUNCTIONS_H
+#define INTERNAL_FUNCTIONS_H
+
+// bibliothèques requises
+#include <stdio.h>
+#include <unistd.h>
+#include "prompt_formatting.h"
+#include "global.h"
+
+//Commandes internes
+typedef struct internal_command
+{
+	char* command_name;
+	int (*internal_function)(command*);
+} internal_command;
+
+
+// execution commandes internes
+int execute(command*);
+//commandes internes
+int cd_slash(command*);
+int pwd_slash(command*);
+int exit_slash(command*);
+
+#endif
