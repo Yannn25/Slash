063612856b72a471cf9b0641e4c8df4d42c9f32d
DESCAMPS antoine <antoine.descamps1213@gmail.com>
2022-11-24 20:33:36 +0100

    Replace main.c

--- main.c
+++ main.c
@@ -9,39 +9,51 @@
 #include "include/global.h"
 
 // paramètrage des variables globales
-int last_return_value = 0;//mettre a 1 pour tester l'affichage du prompt avec erreur
+int last_return_value = 0;
+char old_logical_pwd[MAX_PWD_LENGTH] = {0};
 char logical_pwd[MAX_PWD_LENGTH] = {0};
-command* user_command;
 
 int main(void) 
 {
 	// initialisation
 	int proc;
 	rl_outstream = stderr;
+	strcpy(old_logical_pwd, getenv("OLDPWD"));
 	strcpy(logical_pwd, getenv("PWD"));
 
 	// boucle principale
 	while(1)
 	{
 		// Prompt Slash
-		char* prompt = readline(make_prompt(last_return_value,logical_pwd));
-		if(strlen(prompt) > 0)
-			add_history(prompt);
+		char* slash_prompt = make_prompt(last_return_value, logical_pwd);
+		char* user_prompt;
+		// tant qu'on nous fournit des lignes blanches, on continue à afficher le prompt
+		do
+		{
+			// Si la commande est vide (Ctrl - D)
+			if((user_prompt = readline(slash_prompt)) == NULL)
+			{
+				free(slash_prompt);
+				exit(0);
+			}
+		}while((strlen(erase_whitespaces(user_prompt)) == 0));
+		add_history(user_prompt);
 
 		//On vérifie si l'utilisateur a saisi une commande interne
-		user_command = command_formatting(prompt);
+		command* user_command = command_formatting(user_prompt);
+		free(slash_prompt);
+		free(user_prompt);
 		// Lancement de la commande utilisateur
 		last_return_value = execute(user_command);
-		//char *lrv = ito
 
 		/* Sinon, on vérifie si l'utilisateur a saisi une commande externe
 		(à mettre plutot dans la fct execute)
 		if((proc = fork()) == 0)
 		{
-			execl("/bin/pwd", "pwd", NULL);
+			execvp(...)
 		}
 		*/
-		
+
 		// si ni l'un ni l'autre, message d'erreur et on boucle
 	}
 
