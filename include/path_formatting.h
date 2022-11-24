d7889cf815dfab8a73f4954d760871f8af8da43a
DESCAMPS antoine <antoine.descamps1213@gmail.com>
2022-11-24 20:36:08 +0100

    Upload New File

---
+++ include/path_formatting.h
@@ -0,0 +1,17 @@
+#ifndef PATH_FORMATTING_H
+#define PATH_FORMATTING_H
+
+//bibliothèques requises
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "global.h"
+
+// manipulation chemins
+char* update_logical_pwd(char** parsed_reference, int number_of_entries, char* logical_pwd);
+char* add_folder(char* logical_pwd, char* reference);
+char* delete_last_folder(char* logical_pwd);
+int get_number_of_directories(char* path);
+char** path_parser(char* path);
+
+#endif
